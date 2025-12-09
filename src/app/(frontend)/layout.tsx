bfe1ca9b0c25a15580f8f0d29e025a7aa6d749ce
teovlt <teo.villet2@gmail.com>
2025-12-09 10:56:28 +0100

    Supp non used files and fix used style

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,7 +1,4 @@
 import React from 'react'
-import { cn } from '@/utilities/ui'
-import { GeistMono } from 'geist/font/mono'
-import { GeistSans } from 'geist/font/sans'
 import { InitTheme } from '@/providers/Theme/InitTheme'
 import type { Metadata } from 'next'
 import { getServerSideURL } from '@/utilities/getURL'
@@ -12,8 +9,6 @@ import { Header } from '@/Header/Component'
 import { Providers } from '@/providers'
 import { LoadingProvider } from '@/providers/LoadingProvider'
 
-import './globals.css'
-
 export const metadata: Metadata = {
   title: "Rust-in - Vélos d'occasion",
   description: "Votre spécialiste vélos d'occasion et réparation à Toulouse",
@@ -23,13 +18,7 @@ export const metadata: Metadata = {
 
 export default function FrontendLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div
-      className={cn(
-        GeistSans.variable,
-        GeistMono.variable,
-        'bg-background text-foreground min-h-screen flex flex-col',
-      )}
-    >
+    <div className="bg-background text-foreground min-h-screen flex flex-col">
       <Providers>
         <LoadingProvider>
           <InitTheme />
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
+import { GeistMono } from 'geist/font/mono'
+import { GeistSans } from 'geist/font/sans'
 
 import './globals.css'
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="fr">
-      <body>{children}</body>
+    <html lang="fr" suppressHydrationWarning>
+      <head>
+        <script src="https://tweakcn.com/live-preview.min.js"></script>
+      </head>
+      <body className={`${GeistSans.variable} ${GeistMono.variable}`} suppressHydrationWarning>
+        {children}
+      </body>
     </html>
   )
 }
-
